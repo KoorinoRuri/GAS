f4c89cec2588f751efcef0b067a989944aee4559
KoorinoMakoto <2421566657@qq.com>
2026-05-30 14:52:46 +0800

    添加单例WitchGameplayTags

---
+++ Source/Witch/Private/WitchGameplayTags.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WitchGameplayTags.h"
+#include "GameplayTagsManager.h"
+
+FWitchGameplayTags FWitchGameplayTags::GameplayTags;
+
+void FWitchGameplayTags::InitializeNativeGameplayTags()
+{
+	UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Secondary.Armor"), FString("减少受到的伤害，提升格挡几率"));
+}
---
+++ Source/Witch/Public/WitchGameplayTags.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+
+/**
+ * WitchGameplayTags
+ * 
+ * 包含原生 GameplayTags 的单例，原生：由C++内部创建，并且可以在C++中使用
+ */
+
+struct FWitchGameplayTags
+{
+public:
+	//static 修饰成员函数，实际上和全局函数没什么区别，不许要通过该类的实例来调用
+	static const FWitchGameplayTags& Get(){ return GameplayTags; }
+	static void InitializeNativeGameplayTags();
+protected:
+	
+private:
+	//static 修饰类成员和变量，代表该变量被该类所有成员共享，在整个程序中只有一份实体，被该类的所有对象共同访问，而非每个对象各存一份副本。
+	//静态成员变量存储在全局/静态数据区（.data 或 .bss 段），不在堆或栈上，因此 sizeof(类) 的大小不含静态成员。
+	//它随程序启动而分配空间，随程序结束才释放，生命周期等同于全局变量。
+	static FWitchGameplayTags GameplayTags; 
+};
